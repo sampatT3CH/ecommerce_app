a18671405258d5f8abc7319fafd804161ea361e4
sampatT3CH <138952828+sampatT3CH@users.noreply.github.com>
2023-12-03 16:56:07 +0530

    Update server.js new

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.use('/api/v1/auth', authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, './clirnt/build')))
 app.use("*", function  (req,res) {
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
